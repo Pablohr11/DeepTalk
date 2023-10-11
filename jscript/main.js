d71229b89ccb141e0363161e391db65461e41814
David y Pablo <145053020+DyP-DAW2@users.noreply.github.com>
2023-10-11 13:46:35 +0200

    Echa MediaQuery para el Index

--- jscript/main.js
+++ jscript/main.js
@@ -1,1 +1,19 @@
+function mostrarMenu(){
+    var imgMenu = document.querySelector(".displayMenuOculto");
+    var menu = document.getElementById("menuOculto");
+    var cruz = document.getElementById("cruz");
+    menu.classList.toggle("oculto");
+    imgMenu.classList.remove("displayMenuOculto");
+    cruz.classList.add("displayMenuOculto");
+}
 
+function ocultarMenu(){
+    var imgMenu = document.getElementById("menu");
+    var menu = document.getElementById("menuOculto");
+    var cruz = document.getElementById("cruz");
+    menu.classList.toggle("oculto");
+    imgMenu.classList.add("displayMenuOculto");
+    cruz.classList.remove("displayMenuOculto");
+}
+
+/*Estas dos funciones se usan para mostrar y ocultar el menu del media en el index*/
