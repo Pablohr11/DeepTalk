6e062c088167cb2cdc73265cdf749b201c895150
Pablohr11 <pablo993968@gmail.com>
2023-10-26 12:05:44 +0200

    Pruebas

--- public/jscript/marco.js
+++ public/jscript/marco.js
@@ -3,4 +3,9 @@ function desplegar(pestañaDesplegar){
     var contenedorDesplegar = deplegables.querySelectorAll("div")[pestañaDesplegar];
     contenedorDesplegar.classList.toggle("oculto");
     contenedorDesplegar.classList.toggle("visible");
+
+    var simbolo = document.getElementById("simbolo0");
+    simbolo.classList.toggle("rotado");
+    simbolo.style.transform = 'rotate(90deg)';
+
 }
