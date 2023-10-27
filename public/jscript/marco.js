cb6af5f0099d0564294677e46195c345666d89c5
David y Pablo <145053020+DyP-DAW2@users.noreply.github.com>
2023-10-27 14:28:44 +0200

    Maqueta Con detalles reamatados

--- public/jscript/marco.js
+++ public/jscript/marco.js
@@ -2,5 +2,5 @@ function desplegar(pestañaDesplegar){
     var deplegables = document.getElementById("barraLateral").querySelector("ul");
     var contenedorDesplegar = deplegables.querySelectorAll("div")[pestañaDesplegar];
     contenedorDesplegar.classList.toggle("oculto");
-    contenedorDesplegar.classList.toggle("visible");
+    contenedorDesplegar.classList.toggle("desplegableVisible");
 }
